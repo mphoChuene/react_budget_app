cfbc0ce09623c4a1330d4f2cf1649ce5ede9f740
mphoChuene <mphochuene42@gmail.com>
2023-07-08 15:56:25 +0200

    complete budget app

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
-import ExpenseItem from "./ExpenseItem";
 import { AppContext } from "../context/AppContext";
+import ExpenseItem from "./ExpenseItem";
 
 const ExpenseList = () => {
   const { expenses } = useContext(AppContext);
@@ -12,6 +12,7 @@ const ExpenseList = () => {
           <th scope="col">Department</th>
           <th scope="col">Allocated Budget</th>
           <th scope="col">Increase by 10</th>
+          <th scope="col">Decrease by 10</th>
           <th scope="col">Delete</th>
         </tr>
       </thead>
@@ -19,7 +20,7 @@ const ExpenseList = () => {
         {expenses.map((expense) => (
           <ExpenseItem
             id={expense.id}
-            key={expense.id}
+            key={expense.key}
             name={expense.name}
             cost={expense.cost}
           />
